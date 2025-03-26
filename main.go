b49d097a339b2ed1515c5691c67635b7296560b5
Mury05 <murielleodilone@gmail.com>
2025-03-26 11:49:02 +0100

    implement basic Gin server with endpoint to retrieve album list

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -14,4 +20,15 @@ var albums = []album{
 	{ID: "4", Title: "Mingus Ah Um", Artist: "Charles Mingus", Price: 23.99},
 	{ID: "5", Title: "Kind of Blue", Artist: "Miles Davis", Price: 19.99},
 	{ID: "6", Title: "A Love Supreme", Artist: "John Coltrane", Price: 29.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("albums", getAlbums)
+	router.Run("localhost:8000")
+}
+
+// Obtenir la liste des albums en format json
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
