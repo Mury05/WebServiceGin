0667cfa4fda8e619f8d052d9c9accf5e5b598de0
Mury05 <murielleodilone@gmail.com>
2025-03-26 14:51:16 +0100

    add database connection setup using GORM for MySQL

---
+++ database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/gin_init?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Erreur de connexion à la base de données :", err)
+	}
+	DB = db
+}
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"example/web-service-gin/database"
 	"example/web-service-gin/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	database.Connect()
 	router := gin.Default()
 	routes.AlbumRoutes(router)
 
